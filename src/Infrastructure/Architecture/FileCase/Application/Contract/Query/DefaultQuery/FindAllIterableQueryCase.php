655f72dae84e529efe8c004f2bb2e1db616fea53
GBurkovnikov <gburkovnikov@calltouch.net>
2024-01-06 15:42:44 +0300

    find all query fix

--- src/Infrastructure/Architecture/FileCase/Application/Contract/Query/DefaultQuery/FindAllIterableQueryCase.php
+++ src/Infrastructure/Architecture/FileCase/Application/Contract/Query/DefaultQuery/FindAllIterableQueryCase.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace App\Bundles\AppMakerBundle\Infrastructure\Architecture\FileCase\Application\Contract\Query\DefaultQuery;
 
 use App\Bundles\AppMakerBundle\Infrastructure\Architecture\Dto\ArchitectureFileCaseDto as FileCase;
+use App\Bundles\AppMakerBundle\Infrastructure\Architecture\Dto\ArchitectureFileDto as ArchitectureFile;
 use App\Bundles\AppMakerBundle\Infrastructure\Architecture\FileCase\Application\Contract\ArchitectureCqrsRequestInterface;
 use App\Bundles\AppMakerBundle\Infrastructure\Architecture\FileCase\Application\Contract\DefaultCqrsRequestCase;
 use App\Bundles\AppMakerBundle\Infrastructure\Architecture\FileCase\Application\Contract\NonArchitect\Enum\RequestCqrsType;
@@ -12,6 +13,14 @@ use App\Bundles\InfrastructureBundle\Infrastructure\Helper\ArrayCollection\Colle
 
 class FindAllIterableQueryCase extends DefaultCqrsRequestCase implements ArchitectureFileCaseInterface, ArchitectureCqrsRequestInterface
 {
+    public function create(FileCase $caseParameters): ArchitectureFile
+    {
+        return
+            parent::create($caseParameters)
+                ->setConstructor($this->createConstructorByParameters(null))
+        ;
+    }
+
     public function createDependencies(FileCase $caseParameters): CollectionInterface
     {
         return $this->createCollection();
